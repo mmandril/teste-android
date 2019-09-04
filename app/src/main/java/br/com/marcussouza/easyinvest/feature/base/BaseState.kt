17db796d584af77d2c85ca6d38d358068cecf7e5
Marcus Souza <marcussouza@Marcuss-MacBook-Pro.local>
2019-09-04 00:26:31 -0300

    Create home layout

--- app/src/main/java/br/com/marcussouza/easyinvest/feature/base/BaseState.kt
+++ app/src/main/java/br/com/marcussouza/easyinvest/feature/base/BaseState.kt
@@ -3,4 +3,6 @@ package br.com.marcussouza.easyinvest.feature.base
 open class BaseState {
     object ShowLoadind : BaseState()
     object HideLoading : BaseState()
+    object ShowError : BaseState()
+    object ShowNoInternet : BaseState()
 }
